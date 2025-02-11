6a79306eb1ff10a9df030ab6ac84ddefad39b7f0
notweerdmonk <wrdmnk@gmail.com>
2025-02-11 15:56:28 +0530

    Fix assembly source

--- items/tiny_elf/programs/asm.s
+++ items/tiny_elf/programs/asm.s
@@ -2,17 +2,17 @@
 .text
 
 _start:
-movl $4, %eax
-xorl %ebx, %ebx
-movl $msg, %ecx
-movl $len, %edx
-int $0x80
+    movl    $4, %eax
+    movl    $1, %ebx
+    movl    $msg, %ecx
+    movl    $len, %edx
+    int     $0x80
 
-movl $1, %eax
-xorl %ebx, %ebx
-int $0x80
+    movl    $1, %eax
+    xorl    %ebx, %ebx
+    int     $0x80
 
 .data
 msg:
-.asciz "hello, world\n"
+    .asciz "hello, world\n"
 .equ len, . - msg
